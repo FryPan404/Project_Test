8ee9c030cf48436a8adf5e4d55c0749e01826cd2
FryPan <nadirachoir@gmail.com>
2021-09-06 18:57:06 -0700

    08:57-7-sept-2021

--- application/views/admin/data_siswa.php
+++ application/views/admin/data_siswa.php
@@ -33,6 +33,7 @@
 
             
                 </div>
+                
             </main>
           <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -139,40 +140,7 @@
   </div>
 </div>
 
-  <footer class="footer text-center"> 2021 © FryPan Admin brought to you by 
-            </footer>
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
-    </div>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-    <script src="<?= base_url('assets/plugins/bower_components/jquery/dist/jquery.min.js')?>"></script>
-    <!-- Bootstrap tether Core JavaScript -->
-    <script src="<?= base_url('assets/bootstrap/dist/js/bootstrap.bundle.min.js')?>"></script>
-    <script src="<?= base_url('assets/js/app-style-switcher.js')?>"></script>
-    <script src="<?= base_url('assets/plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js')?>"></script>
-    <!--Wave Effects -->
-    <script src="<?= base_url('assets/js/waves.js')?>"></script>
-    <!--Menu sidebar -->
-    <script src="<?= base_url('assets/js/sidebarmenu.js')?>"></script>
-    <!--Custom JavaScript -->
-    <script src="<?= base_url('assets/js/custom.js')?>"></script>
-    <!--This page JavaScript -->
-    <!--chartis chart-->
-    <script src="<?= base_url('assets/plugins/bower_components/chartist/dist/chartist.min.js')?>"></script>
-    <script src="<?= base_url('assets/plugins/bower_components/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js')?>"></script>
-    <script src="<?= base_url('assets/js/pages/dashboards/dashboard1.js')?>"></script>
-  <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-
+  
             <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
--- application/views/error.php
+++ application/views/error.php
@@ -5,8 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- <link rel="stylesheet" href="<?php echo base_url('assets/css/glitch.css') ?>"> -->
+    <style>
+        body{
+    background: #222;
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    color: white;
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+    
+}
+.glitch{
+    font-size: 4rem;
+    font-weight: 700;
+    text-transform: uppercase;
+
+    text-shadow: 
+    0.03em 0 0 rgba(255,0,0,.75),
+    -0.03em -0.020em 0 rgba(0,255,0,.75),
+    0.020em 0.03em 0 rgba(0,0,255,.75);
+}
+    </style>
+
 </head>
 <body>
-    Error mhank
+    <h3><a class="glitch"> Error! You Haven't Access</a></h3>
+    
 </body>
 </html>
